b868ceb9f13c5de4b293e51da55a420e01b7821b
Kirti <kirti.bora09@gmail.com>
2021-03-11 23:19:42 +0530

    List and Dictionary examples

---
+++ Session07.py
@@ -0,0 +1,10 @@
+varlist =[{'name': 'xyz', 'phone': 12343232, 'address': 'Pune','dob': '24/12/2016','education' : 'abc' }]
+print (varlist)
+varlist[0].update({'phone_no' : '12122323'})
+varlist.append('New Item')
+varlist.append({'K1': 'V1'})
+print (varlist)
+vardict ={'employee_no': '12', 'emp_id': 'emp_123232', 'name': 'emp name', 'skill_sets': ['QA', 'Python', 'SQL'],'dept_id' : 'D1' }
+print(vardict)
+vardict.update({'Sal': 10000,'Role' : 'DBA' , 'Exp' : '6 Years'})
+print(vardict)
